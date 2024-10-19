a137df6fe826d6d242c6d2e6c7dcd493821db5bc
Abhishek <abhibhovar@gmail.com>
2024-10-19 22:49:27 +0530

    Added urls for new app and added the urls of new app to the base project

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from pprint import pprint
 
 # Create your views here.
+
+def simple_view(requests):
+    pprint(requests.__dict__)
+    return HttpResponse("Hello")
