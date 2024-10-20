be7c3649e321fb1b9324f4462072a28e2883f8f7
Abhishek <abhibhovar@gmail.com>
2024-10-20 11:24:11 +0530

    Added sample posts data and view to return a HttpResponse(html)

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('simpleview/', views.simple_view) # URL and the view that will run once URL called
+    path('simpleview/', views.simple_view), # URL and the view that will run once URL called
+    path('simpleview/', views.simple_view2),
+    path('htmlresponse/', views.sample_html_resp_view)
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from pprint import pprint
+import json
+
+# keeping sample data ready for temp purpose will be replaced with actual data once database ready
+sample_posts = json.loads(open(fr"posts\sample_data.json", "r").read())
 
 # Create your views here.
 
 def simple_view(requests):
-    pprint(requests.__dict__)
-    return HttpResponse("Hello")
+    #pprint(requests.__dict__)
+    return HttpResponse("Simple view - 1")
+
+
+# Sample view to check if same route used which view will be called first
+def simple_view2(requests):
+    #pprint(requests.__dict__)
+    return HttpResponse("Simple view - 2")
+
+
+# sample view to understand how to send HTML response
+def sample_html_resp_view(requests):
+    html = ""
+    for post in sample_posts[:5]:
+        html += f"""
+        <div>
+            <span>
+            <h2> {post.get('id')}.) {post.get('title')} </h2>
+            </span>
+            <h4> {post.get('content')} </h4>
+        </div>
+        """
+    return HttpResponse(html)
